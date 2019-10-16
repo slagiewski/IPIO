e81ce0dc1c1b0528fd5a20c1abf0feb8503bb0b9
Nadrill <pawel.gluszczak1@gmail.com>
2019-10-16 20:02:25 +0200

    * Saving of modified image * Minor UI changes

--- IPIO/MainWindow.xaml.cs
+++ IPIO/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using IPIO.Extensions;
 using Microsoft.Win32;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -10,11 +11,13 @@ namespace IPIO
     public partial class MainWindow : Window
     {
         private IWatermarkingAlgorithm _algorithm;
+        private Bitmap _modifiedImage = null;
         private const string IMG_FILE_FILTERS = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg";
-
+        private const string SAVE_IMG_FILE_FILTERS = "Image files (*.png)|*.png";
         public MainWindow()
         {
             InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             //create alg factory
             _algorithm = new DimAlgorithm();
         }
@@ -34,23 +37,43 @@ namespace IPIO
                 ImageBefore.Source = bm.ToImage();
 
                 var modifiedImageBitmap = await Task.Run(() => _algorithm.Run(bm));
+                _modifiedImage = modifiedImageBitmap;
                 ImageAfter.Source = modifiedImageBitmap.ToImage();
-               
+
                 HideProgressBar();
+                ChangeEnableStateSaveButton(true);
             }
         }
-
-        private void HideProgressBar()
+        private async void SaveFileButton_Click(object sender, RoutedEventArgs e)
         {
-            ProgressBar.Visibility = Visibility.Hidden;
+            if (_modifiedImage == null) return;
+            var dialog = new SaveFileDialog
+            {
+                Filter = SAVE_IMG_FILE_FILTERS
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                await Task.Run(() => _modifiedImage.Save(dialog.FileName, ImageFormat.Png));
+                ClearWindow();
+            }
+
         }
+
+        private void ChangeEnableStateSaveButton(bool enable) => SaveButton.IsEnabled = enable;
+        private void HideProgressBar() => ProgressBar.Visibility = Visibility.Hidden;
 
         private void InitProgressBar()
         {
             ProgressBar.Visibility = Visibility.Visible;
-            ProgressBar.Height = 30;
+            ProgressBar.Height = 50;
             ProgressBar.Width = 200;
             ProgressBar.IsIndeterminate = true;
+        }
+
+        private void ClearWindow()
+        {
+            ImageAfter.Source = null;
+            ChangeEnableStateSaveButton(false);
         }
     }
 }
