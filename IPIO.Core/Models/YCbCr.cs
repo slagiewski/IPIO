737bb5dcbb69a09831ac202351a84492f4b4ef31
Nadrill <pawel.gluszczak1@gmail.com>
2019-11-05 23:27:53 +0100

    in progress ycbcr

---
+++ IPIO.Core/Models/YCbCr.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPIO.Core.Models
+{
+    public struct YCbCr
+    {
+        public float Y { get; set; }
+        public readonly float Cb { get; }
+        public readonly float Cr { get; }
+        public readonly int Row { get; }
+        public readonly int Column { get; }
+
+        public YCbCr(float y, float cb, float cr)
+        {
+            Y = y;
+            Cb = cb;
+            Cr = cr;
+            Row = 0;
+            Column = 0;
+        }
+        public YCbCr(float y, float cb, float cr, int row, int column)
+        {
+            Y = y;
+            Cb = cb;
+            Cr = cr;
+            Row = row;
+            Column = column;
+        }
+
+
+    }
+}
